84ebd81d58b93353d2ad5b55c8f440f19b8ba132
AttackPatrick <>
2023-08-08 18:17:24 +0000

    Initial setup, clear screen function and basic imports

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+import json
+
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
